4a630e00a0a0b93fb4c3707281bbb727140ac6bb
Khat Ellingsen <32379479+carlcat01@users.noreply.github.com>
2024-05-06 22:14:59 -0600

    Added multi packet frames
    Attempt to add multi-packet frames using offset.

--- components/ddp/ddp.cpp
+++ components/ddp/ddp.cpp
@@ -81,12 +81,17 @@ bool DDPComponent::process_(const uint8_t *payload, uint16_t size) {
   // ignore packet if data offset != [00 00 00 00].  This likely means the device is receiving a DDP packet not meant for it.
   // There may be a better way to handle this header field.  One user was receiving packets with non-zero data offset that were
   // screwing up the light effect (flickering).
-  if ( payload[4] || payload[5] || payload[6] || payload[7] ) {
-    ESP_LOGE(TAG, "Ignoring DDP Packet with non-zero data offset.");
-    return false;
-  }
+
+  //Change, offset is needed for multi-packet frames. Offset value will reflect the starting channel point where the packet should be applied. 
+  //if ( payload[4] || payload[5] || payload[6] || payload[7] ) {
+  //  ESP_LOGE(TAG, "Ignoring DDP Packet with non-zero data offset.");
+  //  return false;
+  //}
 
   ESP_LOGV(TAG, "DDP packet received (size=%d): - %02x %02x %02x %02x %02x %02x %02x %02x %02x %02x [%02x %02x %02x]", size, payload[0], payload[1], payload[2], payload[3], payload[4], payload[5], payload[6], payload[7], payload[8], payload[9], payload[10], payload[11], payload[12] );
+
+//get offset value as int
+  uint16_t offsetnum = uint16_t((uint8_t)payload[4] << 24 | (uint8_t)payload[5] << 16 | (uint8_t)payload[6] << 8 | (uint8_t)payload[7]);
 
   // first 10 bytes are the header, so consider them used from the get-go
   // if timecode field is used, takes up an additional 4 bytes of header.
@@ -94,7 +99,8 @@ bool DDPComponent::process_(const uint8_t *payload, uint16_t size) {
   // where the timecode field is included and cannot be removed, this may need
   // modified to handle the timecode field.  So far, neither WLED nor xLights
   // follow the header spec in general, and neither sends a timecode field.
-  uint16_t used = 10;
+  //add offset to used for later packets
+  uint16_t used = 10 + offsetnum;
 
   // run through all registered effects, each takes required data per their size starting at packet address determined by used.
   for (auto *light_effect : this->light_effects_) {
