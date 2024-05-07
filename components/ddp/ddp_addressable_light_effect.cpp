29ae21e75f35fb1fef4c26393d4373602476b64e
Carl Ellingsen <32379479+carlcat01@users.noreply.github.com>
2024-05-06 23:42:11 -0600

    Update ddp_addressable_light_effect.cpp

--- components/ddp/ddp_addressable_light_effect.cpp
+++ components/ddp/ddp_addressable_light_effect.cpp
@@ -103,16 +103,14 @@ uint16_t DDPAddressableLightEffect::process_(const uint8_t *payload, uint16_t si
 
 //check if packet is later, if so increae number of pixels
 uint16_t offsetnum = uint16_t((uint8_t)payload[4] << 24 | (uint8_t)payload[5] << 16 | (uint8_t)payload[6] << 8 | (uint8_t)payload[7]);
- uint16_t start_index = used + (offsetnum/3);
- uint16_t end_index = num_pixels + (offsetnum/3);
   //if ( offsetnum > 0 ) { return 0; }
   if ( num_pixels < 1 ) { return 0; }
 
-  ESP_LOGV(TAG, "Applying DDP data for '%s' (size: %d - used: %d - num_pixels: %d) {offset %d - start: %d - end: %d}", get_name().c_str(), size, used, num_pixels, offsetnum, start_index, end_index);
+  ESP_LOGV(TAG, "Applying DDP data for '%s' (size: %d - used: %d - num_pixels: %d) {offset %d}", get_name().c_str(), size, used, num_pixels, offsetnum);
 
   // will be multiplied by RGB values in scale_* scaling modes
   float multiplier = 1.0f;
-
+/*
   // grab scaling multiplier for packet or strip level
   // max out brightness in all but multiply mode, in which brightness is used.
   switch (this->scaling_mode_) {
@@ -130,7 +128,7 @@ uint16_t offsetnum = uint16_t((uint8_t)payload[4] << 24 | (uint8_t)payload[5] <<
     default:
       break; // Multiply mode is default ESPHome behavior, no need to do anything to handle it.
   }
-  if ( offsetnum > 0 ) { return 0; }
+*/
 
   // loop through all pixels being displayed now.
   for (uint16_t i = used; i < used+(num_pixels*3); i+=3) {
